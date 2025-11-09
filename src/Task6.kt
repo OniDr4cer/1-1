2c75b4e670b542b53ee5de0da5365b85c2dac7c9
Махмуд <@>
2025-11-09 22:49:03 +0300

    Complete task 6

--- src/Task6.kt
+++ src/Task6.kt
@@ -1,13 +1,13 @@
 fun main(){
     print("Поиск нечетного числа\n")
     print("Введите число a = ")
-    val a = readln().toInt()
+    val a = readln().toString()
 
     print("Введите число b = ")
-    val b = readln().toInt()
+    val b = readln().toString()
 
-    val ab = a * 10 + b
-    val ba = b * 10 + a
+    val ab = (a + b).toInt()
+    val ba = (b + a).toInt()
 
     if (ab % 2 == 1){
         println("a + b = $ab")}
